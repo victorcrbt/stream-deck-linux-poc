a43b54c5047e5ad70e2beac73efdf7d73e70ab64
Victor Batalha <victorcb_@live.com>
2025-12-07 18:59:46 -0300

    chore: adjust styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,28 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+
+  const generateTiles = (size: number) => {
+    return Array.from({ length: size }, (_, index) => index)
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="wrapper">
+      <h1 className="title">Stream Deck</h1>
+
+      <header className="header">
+        <button>x</button>
+      </header>
+
+      <main className="tiles-container">
+        {generateTiles(15).map((tile) => (
+          <button key={tile} className="tile-button">
+            {tile}
+          </button>
+        ))}
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
